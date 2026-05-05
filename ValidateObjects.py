ff5234a5e4608da5042ce666d66daf0e459a83fb
Aster <aster.sala.cybulski@student.howest.be>
2026-05-05 16:09:28 +0200

    Create a class "ValidateScene" to create an operator to check for scaling issues by looping through all meshes in my scene

--- ValidateObjects.py
+++ ValidateObjects.py
@@ -9,7 +9,26 @@ bl_info = {
 
 import bpy
 
-#class ValidateScene(bpy.types.Operator):    
+class ValidateScene(bpy.types.Operator):
+    # I want to create a custom operator for this tool
+    bl_idname = "scene.validate_scene"
+    bl_label = "Validate Scene"
+    
+    def execute(self, context):
+        # This will run the following script when the user clicks the button
+        # We want to start by looping over every object in our scene and making sure it goes only through meshes
+        for obj in context.scene.objects:
+            if obj.type != 'MESH':
+                continue
+            
+            if obj.scale != (1,1,1):
+                self.report({'WARNING'}, f"{obj.name}: has an unapplied scale!")
+                print(f"WARNING: {obj.name} has an unapplied scale!")
+                #If our object doesn't have a correct scale applied it shows a warning
+            
+        return {'FINISHED'}
+        
+        
     
 class Display_ValidateScene(bpy.types.Panel):
     # We prepare our panel where our tool will be displayed
@@ -22,18 +41,18 @@ class Display_ValidateScene(bpy.types.Panel):
     def draw(self, context):
         # We make out layout and we use the custom operator we've created on our previous class
         layout = self.layout
-        #layout.operator("scene.validate_scene")
+        layout.operator("scene.validate_scene")
 
 
 
 # For each class we make and want to run, we must register and unregister them in this format
         
 def register():
-    #bpy.utils.register_class(ValidateScene)
+    bpy.utils.register_class(ValidateScene)
     bpy.utils.register_class(Display_ValidateScene)
 
 def unregister():
-    #bpy.utils.unregister_class(ValidateScene)
+    bpy.utils.unregister_class(ValidateScene)
     bpy.utils.unregister_class(Display_ValidateScene)
 
 if __name__ == "__main__":
